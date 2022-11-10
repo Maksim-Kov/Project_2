6aef9f2768243a181cb780a2e1cda6d93f0d9940
Maksim-Kov <mkovalev1980@gmail.com>
2022-11-11 01:44:48 +0200

    11/11/2022

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-// document.write(10000)
-// $(document).ready(function() {
-// 	$('.icon-menu').click(function(event) {
-// 		$('.icon-menu, .menu__body').toggleClass('active');
-// 	});
-// });
 
 let menuBtn = document.querySelector('.icon-menu');
 let menu = document.querySelector('.menu__body');
