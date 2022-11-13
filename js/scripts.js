18637738061c440bf05b912f36efeac5a7502dc1
Maksim-Kov <mkovalev1980@gmail.com>
2022-11-13 13:12:27 +0200

    13/11/2022

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,24 @@ menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+
+
+// let button = document.querySelectorAll('btn');
+
+// button.forEach (btn => {
+// 	btn.addEventListener('click', function () {
+// 		button.forEach(butt => butt.classList.remove('active'));
+// 		this.classList.add('active');
+// 	})
+// })
+
+
+// let link = document.querySelector('.btn');
+// link.addEventListener('click', function(){
+// 	link.classList.toggle('active');
+// })
+
+// let link = document.querySelector('.btn');
+// link.onclick = (Event) => {
+// 	Event.target.classList.add('active')
+// }
